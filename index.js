cd533d296f948bb91298da431aa7d43c88bf179b
meetsudarshan <122917643+meetsudarshan@users.noreply.github.com>
2023-07-06 20:48:42 -0400

    made the logo text bigger and made some small corrections in index.js including adding comments

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./utils/generateSVG.js');
 
+// Function to run the logo generator
 const runLogoGenerator = async () => {
+  // Prompt the user with questions to gather logo information
   const logoInfo = await inquirer.prompt(userQuestions);
 
-  let selectedShape;
+  let selectedShape; // Variable to store the selected shape object
 
   switch (logoInfo.shape) {
     case 'triangle':
+      // Create a new Triangle object with user-provided information
       selectedShape = new Triangle(logoInfo.text, logoInfo.color, logoInfo.background);
       break;
     case 'circle':
+      // Create a new Circle object with user-provided information
       selectedShape = new Circle(logoInfo.text, logoInfo.color, logoInfo.background);
       break;
     case 'square':
+      // Create a new Square object with user-provided information
       selectedShape = new Square(logoInfo.text, logoInfo.color, logoInfo.background);
       break;
     default:
@@ -22,8 +27,10 @@ const runLogoGenerator = async () => {
       return;
   }
 
+  // Generate SVG content for the selected shape
   const svgContent = selectedShape.generateSvgContent();
 
+  // Write the SVG content to a file
   fs.writeFile('./Assets/logo.svg', svgContent, err => {
     if (err) {
       console.error('Error writing SVG file:', err);
@@ -33,28 +40,30 @@ const runLogoGenerator = async () => {
   });
 };
 
+// Questions to prompt the user for logo information
 const userQuestions = [
   {
     type: 'input',
-    message: 'Please enter 3 characters.',
+    message: 'Please enter 3 characters for your logo:',
     name: 'text'
   },
   {
     type: 'input',
-    message: 'Please enter a color for your characters.',
+    message: 'Please enter a color for the characters:',
     name: 'color'
   },
   {
     type: 'list',
-    message: 'Select a shape for your logo?',
+    message: 'Select a shape for your logo:',
     name: 'shape',
     choices: ['triangle', 'circle', 'square']
   },
   {
     type: 'input',
-    message: 'Please enter a color for the background of your logo.',
+    message: 'Please enter a background color for your logo:',
     name: 'background'
   }
 ];
 
+// Run the logo generator
 runLogoGenerator();
--- utils/generateSVG.js
+++ utils/generateSVG.js
@@ -1,32 +1,47 @@
 class Shape {
-    constructor(text, color, background) {
-      this.text = text;
-      this.color = color;
-      this.background = background;
-    }
-  
-    generateSvgContent() {
-      throw new Error('Method not implemented.');
-    }
+  constructor(text, color, background) {
+    this.text = text;
+    this.color = color;
+    this.background = background;
   }
-  
-  class Circle extends Shape {
-    generateSvgContent() {
-      return `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"><circle cx="50%" cy="50%" r="40%" fill="${this.background}" /><text x="50%" y="50%" fill="${this.color}" text-anchor="middle">${this.text}</text></svg>`;
-    }
+
+  generateSvgContent() {
+    throw new Error('Method not implemented.'); // Placeholder method that should be implemented in subclasses
   }
-  
-  class Triangle extends Shape {
-    generateSvgContent() {
-      return `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"><polygon points="50,0 100,100 0,100" fill="${this.background}" /><text x="50" y="66" fill="${this.color}" text-anchor="middle">${this.text}</text></svg>`;
-    }
+}
+
+class Circle extends Shape {
+  generateSvgContent() {
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+      <circle cx="50%" cy="50%" r="40%" fill="${this.background}" /> <!-- Circle shape with specified attributes -->
+      <text x="50%" y="50%" fill="${this.color}" text-anchor="middle" font-size="80">${this.text}</text> <!-- Text positioned at the center of the circle -->
+    </svg>`;
   }
-  
-  class Square extends Shape {
-    generateSvgContent() {
-      return `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200"><rect width="80%" height="80%" x="10%" y="10%" fill="${this.background}" /><text x="50%" y="50%" fill="${this.color}" text-anchor="middle">${this.text}</text></svg>`;
-    }
+}
+
+class Triangle extends Shape {
+  generateSvgContent() {
+    const svgWidth = 300;
+    const svgHeight = 200;
+    const triangleHeight = svgHeight * 0.866; // Height of an equilateral triangle
+
+    const centerX = svgWidth / 2;
+    const textY = (svgHeight - triangleHeight) / 2 + triangleHeight * 0.75; // Adjusted y position for the text
+
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
+      <polygon points="${centerX},${(svgHeight - triangleHeight) / 2} ${svgWidth},${svgHeight} 0,${svgHeight}" fill="${this.background}" /> <!-- Triangle shape with specified points -->
+      <text x="${centerX}" y="${textY}" fill="${this.color}" text-anchor="middle" font-size="80">${this.text}</text> <!-- Text positioned at the center of the triangle -->
+    </svg>`;
   }
-  
-  module.exports = { Circle, Triangle, Square };
-  
+}
+
+class Square extends Shape {
+  generateSvgContent() {
+    return `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+      <rect width="80%" height="80%" x="10%" y="10%" fill="${this.background}" /> <!-- Square shape with specified attributes -->
+      <text x="50%" y="55%" fill="${this.color}" text-anchor="middle" font-size="80">${this.text}</text> <!-- Text positioned at the center of the square -->
+    </svg>`;
+  }
+}
+
+module.exports = { Circle, Triangle, Square };
